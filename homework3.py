9116a420309eee24b452ea71023086bcc36624f7
ElenaNovikova1978 <languagebridge@mail.ru>
2024-06-05 12:00:17 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,13 @@
+name ='Name: Elena'
+print(name)
+age= 'Age: 45'
+print (age)
+age=45
+new_age=(age)+1
+print( new_age)
+new_age='New Age: 46'
+print( new_age)
+is_student= 46>45
+print (is_student)
+name='Is_student: True'
+print (name)
